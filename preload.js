21a18e433c63fb462e745f37763c0afe0e2f3659
Artim-Nayas <sayan.mitra@getsimplifin.com>
2026-06-05 21:07:12 +0530

    feat: get-contributed-repos IPC (repos contributed to, no open PR needed)

--- preload.js
+++ preload.js
@@ -4,6 +4,7 @@ contextBridge.exposeInMainWorld('api', {
   getMyPRs: () => ipcRenderer.invoke('get-my-prs'),
   getReviewRequests: () => ipcRenderer.invoke('get-review-requests'),
   getContributions: () => ipcRenderer.invoke('get-contributions'),
+  getContributedRepos: () => ipcRenderer.invoke('get-contributed-repos'),
   openExternal: (url) => ipcRenderer.send('open-external', url),
   updateTrayCount: (count) => ipcRenderer.send('update-tray-count', count),
   quitApp: () => ipcRenderer.send('quit-app')
